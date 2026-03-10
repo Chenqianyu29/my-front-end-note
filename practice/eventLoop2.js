b74843cf647bfea058581546800baba30ae3e66e
xiezilailai22 <xiaolupluto@outlook.com>
2026-03-10 15:34:12 +0800

    update practice

--- practice/eventLoop2.js
+++ practice/eventLoop2.js
@@ -3,7 +3,7 @@ async function async1() {
   console.log("async1 start");
   await async2();
   await async3();
-  console.log("async1 end");
+  console.log("async1 end"); // 微3
 }
 
 async function async2() {
@@ -17,7 +17,7 @@ async function async3() {
 console.log("script start");
 
 setTimeout(function () {
-  console.log("setTimeout");
+  console.log("setTimeout"); // 宏1
 }, 0);
 
 async1();
@@ -26,7 +26,17 @@ new Promise(function (resolve) {
   console.log("promise1");
   resolve();
 }).then(function () {
-  console.log("promise2");
+  console.log("promise2"); // 微2
 });
 
 console.log("script end");
+
+// script start
+// async1 start
+// async2
+// promise1
+// script end
+// async3
+// promise2
+// async1 end
+// setTimeout
